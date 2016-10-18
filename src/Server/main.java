ef60bf48587bc8b75cf953a2e51401c9ceede921
James Manzoor <james.manzoor@ic.ac.uk>
2016-10-19 00:09:50 +0100

    Skeleton Project

---
+++ src/Server/main.java
@@ -0,0 +1,11 @@
+package Server;
+
+/**
+ * Created by Genga on 18/10/2016.
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Tests/mainTest.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import org.junit.Test;
+
+
+/**
+ * Created by Genga on 18/10/2016.
+ */
+public class mainTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
