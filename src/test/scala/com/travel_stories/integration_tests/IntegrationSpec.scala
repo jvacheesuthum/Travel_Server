7f07ed5646ebb629f9e7fd26c1fc7c92dc59f958
James Manzoor <james.manzoor@ic.ac.uk>
2016-10-28 00:24:38 +0100

    added testing suite functionallity, writing tests later. \n\n\n spliting functionallity for unit testing vs intergration testing

---
+++ src/test/scala/com/travel_stories/integration_tests/IntegrationSpec.scala
@@ -0,0 +1,8 @@
+package com.travel_stories.integration_tests
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Genga on 27/10/2016.
+  */
+abstract class IntegrationSpec extends FlatSpec with Matchers
---
+++ src/test/scala/com/travel_stories/unit_tests/UnitSpec.scala
@@ -0,0 +1,8 @@
+package com.travel_stories.unit_tests
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Genga on 27/10/2016.
+  */
+abstract class UnitSpec extends FlatSpec with Matchers
