66b72c27d7a67dc49b2d2a83441f491806eec1e5
James Manzoor <james.manzoor14@imperial.ac.uk>
2016-11-02 20:35:17 +0000

    TravelClient test update

--- src/main/scala/com/travel_stories/TravelClient.scala
+++ src/main/scala/com/travel_stories/TravelClient.scala
@@ -10,11 +10,11 @@ object TravelClient {
     val test = "timeline_address:256.0,325.7@542.25,452.5654@541.6,641.36@"
     val client = new Client("http://cloud-vm-46-251.doc.ic.ac.uk:" + serverPort)
 
-    client.connectBlocking();
-    client.send(test);
-    while (true){
+    println("HIII")
+    client.connectBlocking()
+    println("HIII")
 
-    }
+    client.send(test)
     client.closeBlocking();
     println("OKAY?");
 
