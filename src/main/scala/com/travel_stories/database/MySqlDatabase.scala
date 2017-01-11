6fc77e19d44a0d2db8a312d2e3f50b8612973b48
Ho Law <hcl114@ic.ac.uk>
2017-01-11 13:38:05 +0000

    get the key

--- src/main/scala/com/travel_stories/database/MySqlDatabase.scala
+++ src/main/scala/com/travel_stories/database/MySqlDatabase.scala
@@ -233,7 +233,7 @@ class MySqlDatabase extends TravelServerDatabase {
     val result = dbConnection.retreiveQuery("SELECT LAST_INSERT_ID() FROM Trips;")
     println("asked for key")
     if (!result.isEmpty) println("have something: " + result.head.mkString(","))
-    val key = result.head("asdpoifjsapoif").asInstanceOf[BigInt]
+    val key = result.head.get("LAST_INSERT_ID()").asInstanceOf[BigInt]
     println("db stored trip: DONE: key: " + key)
     return key;
   }
