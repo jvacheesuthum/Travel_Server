5283a8ba64f4d967296ae25502247952aa7278dc
James Manzoor <james.manzoor14@imperial.ac.uk>
2017-01-17 14:38:25 +0000

    fixed database test to real example

--- src/test/scala/com/travel_stories/database/MySqlDatabaseIT.scala
+++ src/test/scala/com/travel_stories/database/MySqlDatabaseIT.scala
@@ -15,9 +15,9 @@ class MySqlDatabaseIT extends IntegrationSpec{
 
   "A MySql Database Query for the name with logitude and latitude of an already found place" should
     "be in the database" taggedAs DbTest in {
-    //The British Museum has already been added by countless tests
+    //The Huxley Building has already been added
     val db = dbFixture.mySQLDb
-    db.getName(-0.1269566, 51.5194133) shouldBe "The British Museum"
+    db.getName(  51.4987336, -0.1796177) shouldBe "The Huxley Building"
   }
 
 
